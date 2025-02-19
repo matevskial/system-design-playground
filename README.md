2213458832979056ed67c911155bf7b36d96610c
Aleksandar Matevski <matevskial@gmail.com>
2025-02-19 16:19:31 +0100

    Initial commit
    Contains one maven module application, in the future this moudle might be renamed or for new microservices new modules would be added

---
+++ README.md
@@ -0,0 +1,4 @@
+# System-design-playground
+
+A collection of various microservices(as maven modules) 
+implemented as part of learning system design in context of backend/web development
---
+++ application/src/test/java/com/matevskial/systemdesignplayground/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matevskial.systemdesignplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
