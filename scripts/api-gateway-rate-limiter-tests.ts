b3c0d36bc6c577f3a4fa13e8ad1ffd662f4e7f8f
Aleksandar Matevski <matevskial@gmail.com>
2025-03-07 08:16:38 +0100

    Add parameter to callRateLimitedEndpointThroughApiGateway to specify path variable

--- scripts/api-gateway-rate-limiter-tests.ts
+++ scripts/api-gateway-rate-limiter-tests.ts
@@ -1,13 +1,13 @@
-function callRateLimitedEndpointThroughApiGateway() {
+function callRateLimitedEndpointThroughApiGateway(configuration: string) {
     return fetch(
-        "http://localhost:6060/application/api/v1/ratelimited/configuration2?delay=30s"
+        `http://localhost:6060/application/api/v1/ratelimited/${configuration}?delay`
     );
 }
 
 console.log("api-gateway custom rate limiter tests")
 
 for (let i = 0; i < 4; i++) {
-    callRateLimitedEndpointThroughApiGateway()
+    callRateLimitedEndpointThroughApiGateway("configuration2")
         .then((res) => console.log(`Response of request ${i}: ${res.statusText}`))
         .catch((err) => console.log(`Error response of request ${i}: ${err}`));
 }
