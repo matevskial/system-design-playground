047275a35b1edaf14d394d6354b59920c9c7849e
Aleksandar Matevski <matevskial@gmail.com>
2025-04-24 00:38:50 +0200

    [url-shortener-application] Rename BeansConfig.java to UrlShortenerConfig.java
    Reason: UrlShortenerConfig.java configures bean using the classes from core url shortener

--- url-shortener/url-shortener-application/src/main/java/com/matevskial/systemdesignplayground/urlshortener/config/UrlShortenerConfig.java
+++ url-shortener/url-shortener-application/src/main/java/com/matevskial/systemdesignplayground/urlshortener/config/UrlShortenerConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class BeansConfig {
+class UrlShortenerConfig {
 
     @Bean
     public UrlShortener urlShortener(TSID.Factory tsIdFactory) {
