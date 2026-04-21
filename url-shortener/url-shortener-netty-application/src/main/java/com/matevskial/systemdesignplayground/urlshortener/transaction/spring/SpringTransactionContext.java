0ddf14aed1552ae2ea119bb5691f9b519b11d72d
Aleksandar Matevski <matevskial@gmail.com>
2026-04-21 13:03:18 +0200

    [url-shortener-netty-application] Use Transaction template instead of transactionManager and DefaultTransactionDefinition in SpringTransactionContext
    Commented and unused code in this file is left on purpose, for reference.

--- url-shortener/url-shortener-netty-application/src/main/java/com/matevskial/systemdesignplayground/urlshortener/transaction/spring/SpringTransactionContext.java
+++ url-shortener/url-shortener-netty-application/src/main/java/com/matevskial/systemdesignplayground/urlshortener/transaction/spring/SpringTransactionContext.java
@@ -6,33 +6,46 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.concurrent.Callable;
 
 @RequiredArgsConstructor
 public class SpringTransactionContext implements TransactionContext {
 
-    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate transactionTemplate;
+
+    public SpringTransactionContext(PlatformTransactionManager platformTransactionManager) {
+        this.transactionTemplate = new TransactionTemplate(platformTransactionManager);
+    }
 
     @Override
-    public <R> R doIn(Callable<R> function) throws Exception {
-        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
-        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
-        TransactionStatus status = transactionManager.getTransaction(def);
-        try {
-            R result = function.call();
+    public <R> R doIn(Callable<R> function) {
+        return (R) transactionTemplate.execute((transactionStatus) -> {
             try {
-                transactionManager.commit(status);
+                return function.call();
             } catch (Exception e) {
-                throw new CommitException(e);
+                return new RuntimeException(e);
             }
-            return result;
-        } catch (CommitException e) {
-            throw e;
-        } catch (Exception e) {
-            transactionManager.rollback(status);
-            throw e;
-        }
+        });
+
+//        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+//        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+//        TransactionStatus status = transactionManager.getTransaction(def);
+//        try {
+//            R result = function.call();
+//            try {
+//                transactionManager.commit(status);
+//            } catch (Exception e) {
+//                throw new CommitException(e);
+//            }
+//            return result;
+//        } catch (CommitException e) {
+//            throw e;
+//        } catch (Exception e) {
+//            transactionManager.rollback(status);
+//            throw e;
+//        }
     }
 
     private static class CommitException extends RuntimeException {
